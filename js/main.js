ae49700a5a65e3a9bb9811d2cd56e618779c2793
titoni-andrea <andreatitoni@gmail.com>
2024-05-02 17:40:35 +0200

    creato ciclo for che genera griglia

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 let inizia = document.querySelector(".inizia")
-let difficolta 
+let difficolta
+const griglia = document.querySelector(".griglia")
 
-inizia.addEventListener("click", function() {
+inizia.addEventListener("click", function () {
     difficolta = document.getElementById("difficolta").value
+
+    for (let i = 1; i <= 100; i++) {
+        const quadrato = document.createElement("div");
+        quadrato.classList.add("q-easy")
+
+        griglia.append(quadrato)
+
+        quadrato.addEventListener("click", function () {
+            quadrato.classList.add("clicked")
+        })
+    }
 
 })
