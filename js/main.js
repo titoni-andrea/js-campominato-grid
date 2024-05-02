d8b0e2e3902b4e121149d0326ae0200a9e3b416c
titoni-andrea <andreatitoni@gmail.com>
2024-05-02 18:00:46 +0200

    sistemato il codice di gen griglia con la funzione

--- js/main.js
+++ js/main.js
@@ -4,47 +4,34 @@ const griglia = document.querySelector(".griglia")
 
 inizia.addEventListener("click", function () {
     difficolta = document.getElementById("difficolta").value
+    let nCelle
+    let nomeClasse
 
     if (difficolta == "facile") {
-    griglia.innerHTML = ""
-
-    for (let i = 1; i <= 100; i++) {
-        const quadrato = document.createElement("div");
-        quadrato.classList.add("q-easy")
-
-        griglia.append(quadrato)
-
-        quadrato.addEventListener("click", function () {
-            quadrato.classList.add("clicked")
-            console.log(i)
-        })
-    }
+        nCelle = 100
+        nomeClasse = "q-easy"
+        genGriglia(nCelle, nomeClasse)
     }
     else if (difficolta == "media") {
-        griglia.innerHTML = ""
-
-        for (let i = 1; i <= 81; i++) {
-            const quadrato = document.createElement("div");
-            quadrato.classList.add("q-medium")
-    
-            griglia.append(quadrato)
-    
-            quadrato.addEventListener("click", function () {
-                quadrato.classList.add("clicked")
-                console.log(i)
-            })
-        }
+        nCelle = 81
+        nomeClasse = "q-medium"
+        genGriglia(nCelle, nomeClasse)
     }
-
     else {
+        nCelle = 49
+        nomeClasse = "q-difficult"
+        genGriglia(nCelle, nomeClasse)
+    }
+    
+    function genGriglia(nCelle, nomeClasse) {
         griglia.innerHTML = ""
-        
-        for (let i = 1; i <= 49; i++) {
+
+        for (let i = 1; i <= nCelle; i++) {
             const quadrato = document.createElement("div");
-            quadrato.classList.add("q-difficult")
-    
+            quadrato.classList.add(nomeClasse)
+
             griglia.append(quadrato)
-    
+
             quadrato.addEventListener("click", function () {
                 quadrato.classList.add("clicked")
                 console.log(i)
