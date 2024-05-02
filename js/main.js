27d5caa327fa9dc66ded2563c4fa9c28888970c1
titoni-andrea <andreatitoni@gmail.com>
2024-05-02 17:47:52 +0200

    gestione delle difficoltà con la select

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const griglia = document.querySelector(".griglia")
 inizia.addEventListener("click", function () {
     difficolta = document.getElementById("difficolta").value
 
+    if (difficolta == "facile") {
+    griglia.innerHTML = ""
+
     for (let i = 1; i <= 100; i++) {
         const quadrato = document.createElement("div");
         quadrato.classList.add("q-easy")
@@ -15,6 +18,38 @@ inizia.addEventListener("click", function () {
             quadrato.classList.add("clicked")
             console.log(i)
         })
+    }
+    }
+    else if (difficolta == "media") {
+        griglia.innerHTML = ""
+
+        for (let i = 1; i <= 81; i++) {
+            const quadrato = document.createElement("div");
+            quadrato.classList.add("q-medium")
+    
+            griglia.append(quadrato)
+    
+            quadrato.addEventListener("click", function () {
+                quadrato.classList.add("clicked")
+                console.log(i)
+            })
+        }
+    }
+
+    else {
+        griglia.innerHTML = ""
+        
+        for (let i = 1; i <= 49; i++) {
+            const quadrato = document.createElement("div");
+            quadrato.classList.add("q-difficult")
+    
+            griglia.append(quadrato)
+    
+            quadrato.addEventListener("click", function () {
+                quadrato.classList.add("clicked")
+                console.log(i)
+            })
+        }
     }
 
 })
